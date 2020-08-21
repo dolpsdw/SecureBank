cd38749b78dccb579112122a7e423d027a16abee
dolpsdw <dolpsdw@gmail.com>
2020-08-21 10:16:20 +0200

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from './Pages/login-page/login-page.component';
+import {TransactionsPageComponent} from './Pages/transactions-page/transactions-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'login', component: TransactionsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { TransactionsPageComponent } from './Pages/transactions-page/transactions-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    TransactionsPageComponent
   ],
   imports: [
     BrowserModule,
