ee937a94beaec62ee6048b706f11a7b1dc51dc3e
dolpsdw <dolpsdw@gmail.com>
2020-09-03 10:38:30 +0200

    4 TransactionList View Displays a No Result message

--- src/app/ReusableViewComponents/transaction-list-view/transaction-list-view.component.spec.ts
+++ src/app/ReusableViewComponents/transaction-list-view/transaction-list-view.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TransactionListViewComponent } from './transaction-list-view.component';
 import {MatIconTestingModule} from '@angular/material/icon/testing';
 import {MatButtonModule} from '@angular/material/button';
+import {By} from '@angular/platform-browser';
 
 describe('TransactionListViewComponent', () => {
   let component: TransactionListViewComponent;
@@ -19,12 +20,29 @@ describe('TransactionListViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TransactionListViewComponent);
     component = fixture.componentInstance;
-    component.transactions = [];
+    component.transactions = [{
+      id: 0,
+      date: new Date('2020-09-03T10:33:33.000Z'),
+      amount: 1,
+      fee: -1,
+      description: 'TestTransaction',
+      userId: 1
+    }];
     component.filterState = {text: 'testSearch', dateSort: 'desc'};
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display a message when there is no transactions to display', () => {
+    // Arrange
+    component.transactions = []; // Ensure no transactions to display
+    fixture.detectChanges(); // Trigger update cycle
+    const noResultHtml = fixture.debugElement.query(By.css('.transaction-list__no-result'));
+    // Act
+    // Assert
+    expect(noResultHtml).not.toBeNull();
   });
 });
